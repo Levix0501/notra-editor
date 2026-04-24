a8484cdd05ef03664ace9e0b31c9f1ed361fc8c7
lxj <1176891456@qq.com>
2026-04-25 01:38:47 +0800

    chore: remove orphaned legacy Button and ui-primitive files

--- packages/notra-editor/src/components/ui-primitive/input.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export function Input({ className, ...props }: React.ComponentProps<'input'>) {
-	const classNames = ['tiptap-input', className].filter(Boolean).join(' ');
-
-	return <input className={classNames} {...props} />;
-}
