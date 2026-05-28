cc4b7113b7d2101dc3490c3cfc96ae5f82908c82
lxj <1176891456@qq.com>
2026-05-28 18:56:29 +0800

    feat(editor): expose public API surface

---
+++ packages/editor/src/index.ts
@@ -0,0 +1,10 @@
+export { NotraBubbleMenu } from "./bubble-menu";
+export { NotraEditor } from "./editor";
+export { NotraEditorContent } from "./editor-content";
+export type {
+  NotraEditorInstance,
+  NotraEditorOptions,
+  NotraEditorProps,
+} from "./types";
+export { useNotraEditor } from "./use-notra-editor";
+export type { JSONContent } from "@tiptap/core";
